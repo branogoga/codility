75e6a99e09b79b071cc6d9912c222acb8969f84f
Branislav Goga <Branislav Goga@fox.tatramed.sk>
2019-04-18 17:51:55 +0200

    Add Lesson 15- AbsDistinct and CountTriangles :

--- 15 - AbsDistinct/15 - AbsDistinct.cpp
+++ 15 - AbsDistinct/15 - AbsDistinct.cpp
@@ -23,9 +23,11 @@ int solution(const std::vector<int>& A)
     unsigned distinctCount = 0;
 
     // Maximal uncovered value, always one of the values |A[minIndex]|, |A[maxIndex]|
-    unsigned value = std::max(
+    int value = std::max(
         std::abs(A[minIndex]), std::abs(A[maxIndex])
     );
+
+    int previous_value = 0;
 
     while (minIndex != maxIndex)
     {
@@ -40,6 +42,17 @@ int solution(const std::vector<int>& A)
         {
             --maxIndex;
         }
+
+        previous_value = value;
+
+        value = std::max(
+            std::abs(A[minIndex]), std::abs(A[maxIndex])
+        );
+    }
+
+    if (previous_value != value)
+    {
+        ++distinctCount;
     }
 
     return distinctCount;
@@ -54,9 +67,15 @@ int main()
     assert(1 == solution({ -7, 7 }));
     assert(1 == solution({ -7, -7, 7, 7 }));
     assert(2 == solution({ 1, 2 }));
-    //assert(2 == solution({ 1, 1, 2 }));
-    //assert(2 == solution({ -1, 1, 2 }));
-    //assert(5 == solution({ -5, -3, -1, 0, 3, 6 }));
+    assert(2 == solution({ -2, 1 }));
+    assert(2 == solution({ 1, 1, 2 }));
+    assert(2 == solution({ -1, 1, 2 }));
+    assert(2 == solution({ -2, -1, 1 }));
+    assert(2 == solution({ -2, -1, 1, 1, 2 }));
+    assert(5 == solution({ -5, -3, -1, 0, 3, 6 }));
+
+    // !!!! Overflows when abs is calculated!
+    //assert(3 == solution({ -2147483648, -1, 0, 1 }));
 
     return 0;
 }
--- 15 - CountTriangles/15 - CountTriangles.cpp
+++ 15 - CountTriangles/15 - CountTriangles.cpp
@@ -3,9 +3,98 @@
 
 #include "stdafx.h"
 
+#include "assert.h"
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+bool isTriangular(int a, int b, int c)
+{
+    return (a + b > c)
+        && (a + c > b)
+        && (b + c > a);
+}
+
+int slow_solution(vector<int>& A)
+{
+    if (A.size() < 3)
+    {
+        return 0;
+    }
+
+    std::sort(A.begin(), A.end());
+
+    unsigned minIndex = 0;
+    unsigned middleIndex = 1;
+    unsigned maxIndex = 2;
+
+    int numberOfTriangles = 0;
+
+    for (minIndex = 0; minIndex < A.size() - 2; ++minIndex)
+    {
+        for (middleIndex = minIndex + 1; middleIndex < A.size() - 1; ++middleIndex)
+        {
+            for (maxIndex = middleIndex + 1; maxIndex < A.size(); ++maxIndex)
+            {
+                if (isTriangular(A[minIndex], A[middleIndex], A[maxIndex]))
+                {
+                    ++numberOfTriangles;
+                }
+            }
+
+        }
+    }
+
+    return numberOfTriangles;
+}
+
+int fast_solution(vector<int>& A)
+{
+    if (A.size() < 3)
+    {
+        return 0;
+    }
+
+    std::sort(A.begin(), A.end());
+
+    int numberOfTriangles = 0;
+
+    for (unsigned minIndex = 0; minIndex < A.size() - 2; ++minIndex)
+    {
+        unsigned middleIndex = minIndex + 1;
+        unsigned maxIndex = middleIndex + 1;
+
+        // Find max index, which is still triangular
+        while ((maxIndex < A.size())
+            && isTriangular(A[minIndex], A[middleIndex], A[maxIndex])
+            )
+        {
+            maxIndex++;
+        }
+
+        if (isTriangular(A[minIndex], A[middleIndex], A[maxIndex]))
+        {
+            numberOfTriangles += pow(2, maxIndex - minIndex - 1) - 1;
+        }
+    }
+
+    return numberOfTriangles;
+}
+
+int solution(vector<int>& A)
+{
+    return fast_solution(A);
+}
 
 int main()
 {
+    assert(0 == solution(vector<int>({})));
+    assert(0 == solution(vector<int>({ 1 })));
+    assert(0 == solution(vector<int>({ 1, 1 })));
+    assert(0 == solution(vector<int>({ 1, 1, 100 })));
+    assert(1 == solution(vector<int>({ 1, 1, 1 })));
+    assert(3 == solution(vector<int>({ 1, 1, 1, 1 })));
+    assert(4 == solution(vector<int>({ 10, 2, 5, 1, 8, 12 })));
     return 0;
 }
-
